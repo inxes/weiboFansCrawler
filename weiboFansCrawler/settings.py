2655c577f84ddbec622ebdad10edae32ebc33d11
wangxinxin <690852990@qq.com>
2019-12-20 19:19:54 +0800

    修复获取fm.view逻辑，增加重试

--- weiboFansCrawler/settings.py
+++ weiboFansCrawler/settings.py
@@ -8,29 +8,10 @@
 #     https://docs.scrapy.org/en/latest/topics/settings.html
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-SPLASH_URL = 'http://localhost:8050'
-
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy_splash.SplashCookiesMiddleware': 723,
-    'scrapy_splash.SplashMiddleware': 725,
-    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
-}
-
-SPIDER_MIDDLEWARES = {
-    'scrapy_splash.SplashDeduplicateArgsMiddleware':100
-}
-
-DUPEFILTER_CLASS ='scrapy_splash.SplashAwareDupeFilter'
-
-HTTPCACHE_STORAGE ='scrapy_splash.SplashAwareFSCacheStorage'
-
-SPLASH_COOKIES_DEBUG = True
-
-SPLASH_LOG_400 = True
-
-SPLASH_SLOT_POLICY = 'scrapy_splash.SlotPolicy.PER_DOMAIN'
 
 BOT_NAME = 'weiboFansCrawler'
+
+HTTPERROR_ALLOWED_CODES = [414]
 
 SPIDER_MODULES = ['weiboFansCrawler.spiders']
 NEWSPIDER_MODULE = 'weiboFansCrawler.spiders'
--- weiboFansCrawler/spiders/fansCrawler.py
+++ weiboFansCrawler/spiders/fansCrawler.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
-from scrapy_splash import SplashRequest
 import scrapy
 import re
 import json
+import time
 
 class FanscrawlerSpider(scrapy.Spider):
     name = 'fansCrawler'
     allowed_domains = ['weibo.com']
+
+    handle_httpstatus_list = [414]
+
+    cus_retry_times = 10
+
+    count = 0
+
+    custom_settings = {
+        "RANDOM_DELAY": 3,
+    }
 
     def start_requests(self):
         # 浏览器用户代理
@@ -15,11 +25,11 @@ class FanscrawlerSpider(scrapy.Spider):
         }
         # 指定cookies
         cookies = {
-            'ALF': '1608343202',
+            'ALF': '1608372361',
             'Apache': '6695636289983.198.1576641109715',
-            'SCF': 'AhYAERVc4btt_ZIfxmIJj-vc2EV8AB2r4pBrS3eneyFe-WbISf5WV6llujr_yCUTctU2BLRukkHYHe4wQ1SYgpE.',
+            'SCF': 'AhYAERVc4btt_ZIfxmIJj-vc2EV8AB2r4pBrS3eneyFeL9Qjmy__hmYX91V7LiU2sQVKtSlS-fDGCvFqY7-ISE0.',
             'SINAGLOBAL': '2164635642128.6316.1553319913398',
-            'SUB': '_2A25w-FtzDeRhGeRG6lcU9C3IzziIHXVTjMu7rDV8PUNbmtANLRPTkW9NUig-Z1JsuhcqfxFu6OnT34sV4udwB0Ag',
+            'SUB': '_2A25w-O1ADeRhGeRG6lcU9C3IzziIHXVTjFmIrDV8PUNbmtAKLU7fkW9NUig-ZwxMtOTvcuWMNPhm4TuXhJlZsmOg',
             'SUBP': '0033WrSXqPxfM725Ws9jqgMF55529P9D9WhrAymGvNJKyKwEjTGQLc8b5JpX5KMhUgL.FozReK-fSheXShB2dJLoI0qLxK-L1-zLB.BLxKqL1-eL12BLxKMLB-eLB-eLxKBLBonL1h5LxKnL1hzLBK-LxKqL1-qLBoBt',
             'SUHB':'0S7DcQkjZzeJ-Z',
             'TC-Page-G0': '51e9db4bd1cd84f5fb5f9b32772c2750|1576727748|1576727741',
@@ -36,21 +46,18 @@ class FanscrawlerSpider(scrapy.Spider):
             'YF-Page-G0': '8438e5756d0e577d90f6ef4db5cfc490|1576663483|1576663316',
             # '': '',
             # '': '',
-            'SSOLoginState': '1576641078',
+            'SSOLoginState': '1576807202',
             'sso_info': 'v02m6alo5qztKWRk5SlkKSQpZCjhKWRk6SljpOUpZCkmKWRk5ilkKOIpZCjjKadlqWkj5OIuIyTlLWNg4y0jYOQwA==',
-            'un': '690852990@qq.com'
+            'un': '690852990@qq.com',
+            's_tentry':'login.sina.com.cn',
+            'wb_view_log_2815543444':'1440*9002',
+            'WBStorage':'42212210b087ca50|undefined',
+            'webim_unReadCount':'%7B%22time%22%3A1576836434093%2C%22dm_pub_total%22%3A0%2C%22chat_group_client%22%3A0%2C%22allcountNum%22%3A0%2C%22msgbox%22%3A0%7D'
         }
 
         urls = [
-            'https://weibo.com/p/1005055448635862/follow?relate=fans&page=1#Pl_Official_HisRelation__59'
+            'https://weibo.com/p/1005055448635862/follow?relate=fans&from=100505&wvr=6&mod=headfans&current=fans#place'
         ]
-
-        splash_args = {
-            'html': 1,
-            'png': 1,
-            'width': 600,
-            'render_all': 1,
-        }
 
         for url in urls:
             yield scrapy.Request(url=url, headers=headers, cookies=cookies, callback=self.parse)
@@ -58,27 +65,54 @@ class FanscrawlerSpider(scrapy.Spider):
     def parse(self, response):
         if not response.body:
             return
-        body = re.findall('<script>FM.view\((.+)\)</script>', response.body.decode())[8]
-        json_body = json.loads(body)['html']
-        virtual_response = scrapy.http.TextResponse(url='', body=json_body.encode())
-        # if virtual_response is None:
-            # TODO 重试
-        next_pages = virtual_response.css('.page.next.S_txt1.S_line1::attr(href)').extract_first()
-        fansids = virtual_response.css("img::attr(usercard)").extract()
-        page = response.url.split("/")[-2]
-        filetxt = '/Users/cindy/Documents/allstar/weibo-crawler/quotes-%s.txt' % page
-        filename = 'quotes-%s.html' % page
-        # fansids = response.css(".S_txt1 a::attr(href)").extract()
-        print('爬取的好友id列表：', fansids)
-        configpath = '/Users/cindy/Documents/allstar/weibo-crawler/config.json'
+        script = re.findall('<script>FM.view\((.+)\)</script>', response.body.decode())
+        print("长度", len(script))
+        if len(script) == 0:
+            print("重试")
+            retries = response.meta.get('cus_retry_times', 0) + 1
+            print("重试次数：", retries)
+            # 打印问题html
+            filename = 'quotes-error.html'
+            if retries <= self.cus_retry_times:
+                r = response.request.copy()
+                r.meta['cus_retry_times'] = retries
+                r.dont_filter = True
+                yield r
+            else:
+                with open(filename, 'wb') as ft:
+                    ft.write(response.body)
+                self.log('Saved file %s' % filename)
+                self.logger.debug("Gave up retrying {}, failed {} times".format(
+                    response.url, retries
+                ))
 
-        print('下一页：' , next_pages)
-        if next_pages is not None:
-            next_page = response.urljoin(next_pages)
-            yield scrapy.Request(next_page, callback=self.parse)
-        for fansid in fansids:
-            print('爬取的好友id：', fansid.split("&")[0][3:])
-            with open(filetxt, 'a+') as ft:
-                ft.write(fansid.split("&")[0][3:]+' ')
-            self.log('Saved file %s' % filetxt)
+        # body = script[8]
+        for body in script:
+            if json.loads(body).get('html') is None:
+                continue
+            json_body = json.loads(body)['html']
+            if json_body is None:
+                continue
+            virtual_response = scrapy.http.TextResponse(url='', body=json_body.encode())
 
+            next_pages = virtual_response.css('.page.next.S_txt1.S_line1::attr(href)').extract_first()
+            # 写入文件
+            fansids = virtual_response.css("img::attr(usercard)").extract()
+            page = response.url.split("/")[-2]
+            filetxt = '/Users/cindy/Documents/allstar/weibo-crawler/quotes-%s.txt' % page
+            # fansids = response.css(".S_txt1 a::attr(href)").extract()
+            print('爬取的好友id列表：', fansids)
+            configpath = '/Users/cindy/Documents/allstar/weibo-crawler/config.json'
+
+            if next_pages is not None:
+                next_page = response.urljoin(next_pages)
+                print('捕获到下一页：', next_page)
+                yield scrapy.Request(next_page, callback=self.parse)
+            for fansid in fansids:
+                print('爬取的好友id：', fansid.split("&")[0][3:])
+                with open(filetxt, 'a+') as ft:
+                    ft.write(fansid.split("&")[0][3:]+' ')
+                self.count = self.count + 1
+                self.log('Saved file %s' % filetxt)
+            time.sleep(1)
+        print('粉丝总数：', self.count)
