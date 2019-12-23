30f3720e69fbe96740f14a7ff9e16066ec178f32
liuhao <alan.liu@yiako.com>
2019-12-23 15:29:59 +0800

    拆分结构

--- weiboFansCrawler/spiders/fansCrawler.py
+++ weiboFansCrawler/spiders/fansCrawler.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# from scrapy_splash import SplashRequest
 import os
 
 import scrapy
@@ -6,9 +7,10 @@ import re
 import json
 import time
 
+
 class FanscrawlerSpider(scrapy.Spider):
     name = 'fansCrawler'
-    #爬网的域的字符串的可选列表
+    # 爬网的域的字符串的可选列表
     allowed_domains = ['weibo.com']
 
     handle_httpstatus_list = [414]
@@ -48,7 +50,7 @@ class FanscrawlerSpider(scrapy.Spider):
             'login_sid_t': 'e85dd926c0edc739aa417b91fef1877f',
             'un': '17621142248',
             'wb_view_log': '1680*10502',
-            'wb_view_log_5322209562':'1680*10502',
+            'wb_view_log_5322209562': '1680*10502',
             'webim_unReadCount': '%7B%22time%22%3A1577067588477%2C%22dm_pub_total%22%3A0%2C%22chat_group_client%22%3A0%2C%22allcountNum%22%3A3%2C%22msgbox%22%3A0%7D',
             # 'TC-Page-G0': '753ea17f0c76317e0e3d9670fa168584|1576995465|1576995464',
             # 'TC-V5-G0': 'eb26629f4af10d42f0485dca5a8e5e20',
@@ -88,7 +90,7 @@ class FanscrawlerSpider(scrapy.Spider):
 
         print("script正则匹配的标签个数长度", len(scripts))
 
-        print("response.URL的值:",response.url)
+        print("response.URL的值:", response.url)
 
         if len(scripts) == 0:
             print("重试")
@@ -102,7 +104,7 @@ class FanscrawlerSpider(scrapy.Spider):
                 r.dont_filter = True
                 yield r
             else:
-                #先截断文件 然后以二进制形式写入文件 默认是t
+                # 先截断文件 然后以二进制形式写入文件 默认是t
                 with open(filename, 'wb') as ft:
                     ft.write(response.body)
                 self.log('Saved file %s' % filename)
@@ -114,7 +116,7 @@ class FanscrawlerSpider(scrapy.Spider):
         # isotimeformat = '%Y%m%d%X'
         #
         # date = time.strftime(isotimeformat, time.localtime(time.time()))
-        filetxt = '/Users/cindy/Documents/allstar/weibo-crawler/quotes-%s-%s.txt' % (page, self.time)
+        filetxt = 'quotes-%s-%s.txt' % (page, self.time)
         # body = script[8]
         for script in scripts:
             if json.loads(script).get('html') is None:
@@ -123,7 +125,7 @@ class FanscrawlerSpider(scrapy.Spider):
             if json_body is None:
                 continue
             virtual_response = scrapy.http.TextResponse(url='', body=json_body.encode())
-            #获取下一页的链接
+            # 获取下一页的链接
             next_pages = virtual_response.css('.page.next.S_txt1.S_line1::attr(href)').extract_first()
             # 写入文件
             fansids = virtual_response.css("img::attr(usercard)").extract()
@@ -131,57 +133,75 @@ class FanscrawlerSpider(scrapy.Spider):
             print('爬取的好友id列表：', fansids)
 
             if next_pages is not None:
-                #urlJoin 处理相对路径
-                next_page = response.urljoin(next_pages)
-                print('捕获到下一页：', next_page)
-                yield scrapy.Request(next_page, callback=self.parse)
-            #粉丝信息写入文件
+                recursivePageInfo(response, next_pages, self)
+            # 粉丝信息写入文件
             for fansid in fansids:
-                print('爬取的好友id：', fansid.split("&")[0][3:])
-                with open(filetxt, 'a+') as ft:
-                    ft.write(fansid.split("&")[0][3:] + ' ')
-                self.count = self.count + 1
-                self.log('Saved file %s' % filetxt)
-
-            #把文件名写入config.json 的user_id_list
+                dealFansId(fansid, filetxt, self)
+            # 把文件名写入config.json 的user_id_list
             configpath = 'config.json'
 
-            #如果文件存在
+            # 如果文件存在
             if os.path.exists(configpath):
-                #读取文件 读取时候文件指针放在文件头部
-                with open(configpath, 'r+') as load_f:
-                    load_dict = json.load(load_f)
-                    print("读取到的json内容:", load_dict)
-                #判断好友id是否存在
-                if fansids is not None and len(fansids) >= 0:
-                    user_id_list = load_dict['user_id_list']
-                    print("user_id_list的集合内容:", user_id_list)
-                    print("var1的类型===", isinstance(user_id_list, list))
-                    #写入的是之前的文件名
-                    if filetxt not in user_id_list:
-                        user_id_list.append(filetxt)
-                        print("user_id_list的集合内容:", user_id_list)
-                        print("拼接的config内容:", load_dict)
-                        json.dump(load_dict, load_f)
+                existDunpFile(configpath, fansids, filetxt)
             else:
-                #文件不存在直接写一个config
-                with open(configpath, 'w') as cf:
-                    #创建赋值
-                    if fansids is not None and len(fansids) >= 0:
-
-                        dict = {'user_id_list': [],
-                                'filter': 1,
-                                'since_date': time.strftime('%Y-%m-%d', time.localtime(time.time())),
-                                'write_mode': ["csv"], 'mysql_config': {
-                                'host': 'localhost', 'port': 3306, 'user': 'root', 'password': '123456',
-                                'charset': 'utf8mb4'
-                            }}
-
-                        print("新创建的config对象",json.dumps(dict))
-                        print("文件名字",filetxt)
-                        user_id_list = dict['user_id_list']
-                        user_id_list.append(filetxt)
-                        json.dump(dict, cf)
-                        print("user_id_list的集合内容:", dict)
+                notExistDumpFile(configpath, fansids, filetxt)
         time.sleep(1)
         print('粉丝总数：', self.count)
+
+
+def dealFansId(fansid, filetxt, self):
+    print('爬取的好友id：', fansid.split("&")[0][3:])
+    with open(filetxt, 'a+') as ft:
+        ft.write(fansid.split("&")[0][3:] + ' ')
+    self.count = self.count + 1
+    self.log('Saved file %s' % filetxt)
+
+
+def recursivePageInfo(response, next_pages, self):
+    # urlJoin 处理相对路径
+    next_page = response.urljoin(next_pages)
+    print('捕获到下一页：', next_page)
+    yield scrapy.Request(next_page, callback=self.parse)
+
+
+# 写入已经存在的文件
+def existDunpFile(configpath, fansids, filetxt):
+    # 读取文件 读取时候文件指针放在文件头部
+    with open(configpath, 'r+') as load_f:
+        load_dict = json.load(load_f)
+        print("读取到的json内容:", load_dict)
+        # 判断好友id是否存在
+        if fansids is not None and len(fansids) >= 0:
+            user_id_list = load_dict['user_id_list']
+            print("user_id_list的集合内容:", user_id_list)
+            # 写入的是之前的文件名
+            if filetxt not in user_id_list:
+                user_id_list.append(filetxt)
+                print("user_id_list的集合内容:", user_id_list)
+                print("拼接的config内容:", load_dict)
+                print("当前文件指针位置", load_f.tell())
+                load_f.seek(0, 0)
+                print("重置后文件指针位置", load_f.tell())
+                json.dump(load_dict, load_f)
+
+
+# 写入不存在的文件
+def notExistDumpFile(configpath, fansids, filetxt):
+    # 文件不存在直接写一个config
+    with open(configpath, 'w') as cf:
+        # 创建赋值
+        if fansids is not None and len(fansids) >= 0:
+            dict = {'user_id_list': [],
+                    'filter': 1,
+                    'since_date': time.strftime('%Y-%m-%d', time.localtime(time.time())),
+                    'write_mode': ["csv"], 'mysql_config': {
+                    'host': 'localhost', 'port': 3306, 'user': 'root', 'password': '123456',
+                    'charset': 'utf8mb4'
+                }}
+
+            print("新创建的config对象", json.dumps(dict))
+            print("文件名字", filetxt)
+            user_id_list = dict['user_id_list']
+            user_id_list.append(filetxt)
+            json.dump(dict, cf)
+            print("user_id_list的集合内容:", dict)
